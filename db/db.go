d396b89b698670f2e37f99fb51d2094b85fcade5
Thomas Martin <thomas.martindev@gmail.com>
2020-05-12 17:45:19 +0200

    cleaning

--- db/db.go
+++ db/db.go
@@ -1,9 +1,6 @@
 package db
 
 import (
-	"encoding/json"
-	"fmt"
-
 	scribble "github.com/nanobox-io/golang-scribble"
 	"primitivo.fr/applinh/GoFire/utils"
 )
@@ -36,36 +33,4 @@ func ReadRes(res string) map[string]map[string]string {
 	value := make(map[string]map[string]string)
 	db.Read("dbItems", res, &value)
 	return value
-}
-
-func DbProcess() {
-	db, _ := scribble.New("./dbItems", nil)
-
-	// add some fish to the database
-	myFishies := []Fish{{Name: "onefish"}, {Name: "twofish"}, {Name: "redfish"}, {Name: "bluefish"}}
-	db.Write("fish", "fish", myFishies)
-	// Read one fish from the database
-	onefish := Fish{}
-	db.Read("fish", "onefish", &onefish)
-
-	fmt.Printf("It's a fish! %#v\n", onefish)
-
-	// Read more fish from the database
-	morefish, _ := db.ReadAll("fish")
-
-	// iterate over morefish creating a new fish for each record
-	fishies := []Fish{}
-	for _, fish := range morefish {
-		f := Fish{}
-		json.Unmarshal([]byte(fish), &f)
-		fishies = append(fishies, f)
-	}
-
-	fmt.Printf("It's a lot of fish! %#v\n", fishies)
-
-	// Delete onefish from the database
-	//db.Delete("fish", "onefish")
-
-	// Delete all fish from the database
-	//db.Delete("fish", "")
 }
