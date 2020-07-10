838a3b03b59c71f3f235063e255d89137a305705
Thomas Martin <thomas.martindev@gmail.com>
2020-07-10 16:42:40 +0200

    error handling is now better

--- db/db.go
+++ db/db.go
@@ -1,11 +1,13 @@
 package db
 
 import (
+	"errors"
+
 	scribble "github.com/nanobox-io/golang-scribble"
 	"primitivo.fr/applinh/GoFire/utils"
 )
 
-func WriteItem(res string, items map[string]interface{}) string {
+func WriteItem(res string, items map[string]interface{}) (string, error) {
 	//toInsert := make(map[string]map[string]string)
 	exisingItems := ReadRes(res)
 	var uuid string
@@ -13,6 +15,8 @@ func WriteItem(res string, items map[string]interface{}) string {
 	if id, exist := items["id"]; exist {
 		if _, exist2 := exisingItems[id.(string)]; !exist2 {
 			uuid = id.(string)
+		} else {
+			return "", errors.New("ID_EXIST")
 		}
 	} else {
 		uuid = utils.GenerateUuid()
@@ -21,15 +25,20 @@ func WriteItem(res string, items map[string]interface{}) string {
 
 	exisingItems[uuid] = items
 	db.Write("dbItems", res, exisingItems)
-	return uuid
+	return uuid, nil
 }
 
-func UpdateItem(res string, id string, item map[string]interface{}) {
+func UpdateItem(res string, id string, item map[string]interface{}) error {
 	exisingItems := ReadRes(res)
+	if _, exist := exisingItems[id]; !exist {
+		return errors.New("ID_NO_EXIST")
+	}
 	exisingItems[id] = item
 
 	db, _ := scribble.New("./dbItems", nil)
-	db.Write("dbItems", res, exisingItems)
+	e := db.Write("dbItems", res, exisingItems)
+
+	return e
 }
 
 func ReadRes(res string) map[string]map[string]interface{} {
